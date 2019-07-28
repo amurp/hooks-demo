102aa909b7af1510998d8876444c9c3e3c91de72
Aaron Murphy <aarongmurphy@gmail.com>
2019-07-28 00:27:59 -0400

    removal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+    >
+      {todo.text}
+      <button onClick={() => completeTodo(index)}>Complete</button>
+      <button onClick={() => removeTodo(index)}>X</button>
+    </div>
+  );
 }
 
 function TodoForm({ addTodo }) {
@@ -47,11 +56,29 @@ function App() {
     setTodos([...todos, { text }]);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
